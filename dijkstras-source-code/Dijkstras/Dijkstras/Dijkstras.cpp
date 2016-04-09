237d0abea341ee6d9163593e3c92e2dcffdc56ae
drmilky8 <andrewpatrickodonnell@gmail.com>
2016-04-09 13:10:31 +0100

    Update Dijkstras.cpp

--- dijkstras-source-code/Dijkstras/Dijkstras/Dijkstras.cpp
+++ dijkstras-source-code/Dijkstras/Dijkstras/Dijkstras.cpp
@@ -41,7 +41,7 @@ vector<Edge*> edges;
 class Node
 {
 public:
-	Node(string id) 
+	Node(string id)
 		: id(id), previous(NULL), distanceFromStart(INT_MAX)
 	{
 		nodes.push_back(this);
@@ -57,7 +57,7 @@ public:
 class Edge
 {
 public:
-	Edge(Node* node1, Node* node2, int distance) 
+	Edge(Node* node1, Node* node2, int distance)
 		: node1(node1), node2(node2), distance(distance)
 	{
 		edges.push_back(this);
@@ -66,9 +66,9 @@ public:
 	{
 		return (
 			(node1 == this->node1 &&
-			node2 == this->node2) ||
-			(node1 == this->node2 && 
-			node2 == this->node1));
+				node2 == this->node2) ||
+			(node1 == this->node2 &&
+				node2 == this->node1));
 	}
 public:
 	Node* node1;
@@ -196,24 +196,26 @@ void DijkstrasTest()
 	Edge* e46 = new Edge(room25, stairs1, 6);
 
 
+
 	
-	cout << "Input the starting point: " << endl;
-	int startID = setStartingPoint();
-	nodes.at(startID)->distanceFromStart = 0; // set start node
 
 
-
-	cout << "Input destination point id: " << endl;
+	//reveresed order to give correct output
+	cout << "Input starting point id: " << endl;
 	int destinationID = setDestinationPoint();
+
+	cout << "Input the destination point: " << endl;
+	int startID = setStartingPoint();
+	nodes.at(startID)->distanceFromStart = 0; // set destination
 
 	Dijkstras();
 
 
 	PrintShortestRouteTo(nodesFINAL.at(destinationID)); //set end node
+	system("pause");
 
 
-
-	// TODO: Node / Edge memory cleanup not included
+														// TODO: Node / Edge memory cleanup not included
 }
 
 ///////////////////
@@ -223,16 +225,16 @@ void Dijkstras()
 	while (nodes.size() > 0)
 	{
 		Node* smallest = ExtractSmallest(nodes);
-		vector<Node*>* adjacentNodes = 
+		vector<Node*>* adjacentNodes =
 			AdjacentRemainingNodes(smallest);
 
 		const int size = adjacentNodes->size();
-		for (int i=0; i<size; ++i)
+		for (int i = 0; i<size; ++i)
 		{
 			Node* adjacent = adjacentNodes->at(i);
 			int distance = Distance(smallest, adjacent) +
 				smallest->distanceFromStart;
-			
+
 			if (distance < adjacent->distanceFromStart)
 			{
 				adjacent->distanceFromStart = distance;
@@ -251,7 +253,7 @@ Node* ExtractSmallest(vector<Node*>& nodes)
 	if (size == 0) return NULL;
 	int smallestPosition = 0;
 	Node* smallest = nodes.at(0);
-	for (int i=1; i<size; ++i)
+	for (int i = 1; i<size; ++i)
 	{
 		Node* current = nodes.at(i);
 		if (current->distanceFromStart <
@@ -271,7 +273,7 @@ vector<Node*>* AdjacentRemainingNodes(Node* node)
 {
 	vector<Node*>* adjacentNodes = new vector<Node*>();
 	const int size = edges.size();
-	for(int i=0; i<size; ++i)
+	for (int i = 0; i<size; ++i)
 	{
 		Edge* edge = edges.at(i);
 		Node* adjacent = NULL;
@@ -295,7 +297,7 @@ vector<Node*>* AdjacentRemainingNodes(Node* node)
 int Distance(Node* node1, Node* node2)
 {
 	const int size = edges.size();
-	for(int i=0; i<size; ++i)
+	for (int i = 0; i<size; ++i)
 	{
 		Edge* edge = edges.at(i);
 		if (edge->Connects(node1, node2))
@@ -310,7 +312,7 @@ int Distance(Node* node1, Node* node2)
 bool Contains(vector<Node*>& nodes, Node* node)
 {
 	const int size = nodes.size();
-	for(int i=0; i<size; ++i)
+	for (int i = 0; i<size; ++i)
 	{
 		if (node == nodes.at(i))
 		{
@@ -325,7 +327,7 @@ bool Contains(vector<Node*>& nodes, Node* node)
 void PrintShortestRouteTo(Node* destination)
 {
 	Node* previous = destination;
-	cout << "Distance from start: " 
+	cout << "Distance from start: "
 		<< destination->distanceFromStart + 1 << endl;
 	while (previous)
 	{
@@ -347,7 +349,7 @@ vector<Edge*>* AdjacentEdges(vector<Edge*>& edges, Node* node)
 	vector<Edge*>* adjacentEdges = new vector<Edge*>();
 
 	const int size = edges.size();
-	for(int i=0; i<size; ++i)
+	for (int i = 0; i<size; ++i)
 	{
 		Edge* edge = edges.at(i);
 		if (edge->node1 == node)
@@ -367,7 +369,7 @@ vector<Edge*>* AdjacentEdges(vector<Edge*>& edges, Node* node)
 void RemoveEdge(vector<Edge*>& edges, Edge* edge)
 {
 	vector<Edge*>::iterator it;
-	for (it=edges.begin(); it<edges.end(); ++it)
+	for (it = edges.begin(); it<edges.end(); ++it)
 	{
 		if (*it == edge)
 		{
@@ -376,4 +378,3 @@ void RemoveEdge(vector<Edge*>& edges, Edge* edge)
 		}
 	}
 }
-
